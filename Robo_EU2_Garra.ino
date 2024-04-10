0fdfdd8094a1db3f44593141d8783e55176194ce
TonyMerino <82270279+TonyMerino@users.noreply.github.com>
2024-04-10 16:05:02 -0300

    Add files via upload

--- Robo_EU2_Garra.ino
+++ Robo_EU2_Garra.ino
@@ -4,7 +4,6 @@
 //Biblioteca do controle de PS4
 #include <PS4Controller.h>
 
-
 //Define os pinos para a locomocão com a Ponte H (TB6612NG)
 #define PWMA 32
 #define PWMB 13
@@ -13,7 +12,8 @@
 #define B1 26
 #define B2 27
 
-#define S1 34
+//Define os pinos para os servos da garra
+#define S1 18
 
 //Define o MAC Address do controle de PS4
 #define MAC_ADDRESS "64:64:64:64:64:64"
@@ -22,6 +22,7 @@
 int posicao1 = 0;
 Servo servo1;
 
+//Função para controlar os motores de locomoção
 void motors_control(int linear, int angular) {
   //Ao somar o angular com linear em cada motor conseguimos a ideia de direção do robô
   int result_R = linear - angular;
@@ -72,20 +73,18 @@ void motor_B(int speedB) {
   Serial.print("\t");
 }
 
-//Controla o primeiro motor da garra
-void garra1(int bt1, int bt2) {
-  
-  if(bt1 == HIGH && posicao1 <=179) {
-    posicao1++;
+//Função para controlar o primeiro motor da garra
+void garra1(bool bt1, bool bt2) {
+  if(bt1 == HIGH && posicao1 < 180) {
+    posicao1 += 1;
     servo1.write(posicao1);
-    delay(5);
+    delay(10);
   }
-  if(bt2 == HIGH && posicao1 >=1) {
-    posicao1--;
+  if(bt2 == HIGH && posicao1 > 0) {
+    posicao1 -= 1;
     servo1.write(posicao1);
-    delay(5);
+    delay(10);
   }
-  
   //Imprime os valores no monitor serial
   Serial.print("Garra1: ");
   Serial.print(posicao1);
@@ -102,19 +101,21 @@ void setup(void) {
   PS4.begin(MAC_ADDRESS);
   Serial.println("Ready.");
   
+  //Define pinos e canais para o PWM
   ledcAttachPin(PWMA,5);
-  ledcAttachPin(PWMB,6);
   ledcSetup(5, 80000, 8);
+  ledcAttachPin(PWMB,6);
   ledcSetup(6, 80000, 8);
 
-  servo1.attach(S1);
+  //Configurações para os motores servo
+  servo1.setPeriodHertz(50); // PWM frequency for SG90
+  servo1.attach(S1, 500, 2400); // Minimum and maximum pulse width (in µs) to go from 0° to 180
   
   //Define os pinos como output
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
   pinMode(B1, OUTPUT);
   pinMode(B2, OUTPUT);
-
   pinMode(S1, OUTPUT);
   
   //Deixa os pinos desligados no início
@@ -123,7 +124,8 @@ void setup(void) {
   digitalWrite(B1, 0);
   digitalWrite(B2, 0);
 
-  digitalWrite(S1, 0);
+  //Deixa os servos na posição inicial (Valor de 0 a 180, a definir pelo robô)
+  servo1.write(0);
   
   while(PS4.isConnected()!= true) {
   delay(20);
@@ -136,9 +138,11 @@ void loop() {
     //Multiplicador = 1.8 para aumentar a velocidade linear, o quão rápido o robô vai ser
     //Multiplicador2 = multiplicador da curva, parâmetro que varia de 1 ate a 2.3 para suavisar as curvas em alta velocidade
     motors_control((1.8)*PS4.LStickY(),(1.3)*PS4.RStickX());
+    //Controle dos motores da garra, dois botões para cada servo
     garra1(PS4.Up(),PS4.Down());
     delay(100);
   }
+
   //Failsafe, se o controle desconectar o robô para
   if(PS4.isConnected()!= true) {
     motors_control(0,0);
